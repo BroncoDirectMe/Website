3fcf500f82634f788b1f9643963263523f07e576
Katie <14coxkm@gmail.com>
2022-11-13 19:15:54 -0800

    Revert "Leaf/samanyu24 x/navbar"

--- src/components/Documentation.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from 'react';
-
-function Documentation(): ReactElement {
-  return <div className="App">This is our documentation page!</div>
-}
-
-export default Documentation;
