54229214e47c805bbcffbea2a61f36491fe10f14
jaroonl <jaronslin@gmail.com>
2022-12-26 19:23:54 -0800

    added code needed to run credits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement } from 'react';
-import Logo from './logo.svg';
 import './App.css';
-import Navbar  from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Documentation from './components/Documentation';
 import Progress from './components/Progress';
@@ -10,31 +9,16 @@ import Credits from './components/Credits';
 
 function App(): ReactElement {
   return (
-
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/documentation' element={<Documentation />} />
-          <Route path='/progress' element={<Progress />} />
-          <Route path='/credits' element={<Credits />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/progress" element={<Progress />} />
+          <Route path="/credits" element={<Credits />} />
         </Routes>
       </Router>
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
