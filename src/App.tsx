0895fdf8f5167244bb66dbd984df5a20848272bb
jaroonl <112691692+jaroonl@users.noreply.github.com>
2022-12-28 19:40:58 -0800

    website credits page (#30)
    * added code needed to run credits page
    
    * removed unnecessary line
    
    * removed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import Logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -30,20 +29,6 @@ function App(): ReactElement {
           <Route path="/credits" element={<Credits />} />
         </Routes>
       </Router>
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
       <footer>
         <Stack
