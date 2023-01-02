c705d4fea0ae9a06f24ece12f94c5aa1e763ee14
GuyWhoCode <thatguywhocodes@gmail.com>
2023-01-02 10:00:42 -0800

    Added responsive footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,14 @@ import Home from './components/Home';
 import Documentation from './components/Documentation';
 import Progress from './components/Progress';
 import Credits from './components/Credits';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Divider,
-  Link,
-  Stack,
-  Typography,
-} from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+const devBuild = true;
 
 function App(): ReactElement {
   return (
     <div className="App">
-      <Router basename="/Website">
+      <Router basename={devBuild ? '/' : '/Website'}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -30,70 +21,7 @@ function App(): ReactElement {
           <Route path="/credits" element={<Credits />} />
         </Routes>
       </Router>
-      <footer>
-        <Stack
-          direction="row"
-          spacing={6}
-          divider={
-            <Divider
-              orientation="vertical"
-              flexItem
-              sx={{ borderRightWidth: 3, borderColor: 'black' }}
-            />
-          }
-        >
-          <Link variant="h6" color="black" underline="none" href="/">
-            Home
-          </Link>
-          <Link variant="h6" color="black" underline="none" href="progress">
-            Progress
-          </Link>
-          {/* Add download link here later */}
-          <Link variant="h6" color="black" underline="none" href="/">
-            Download
-          </Link>
-          <Link
-            variant="h6"
-            color="black"
-            underline="none"
-            href="documentation"
-          >
-            Docs
-          </Link>
-          <Link variant="h6" color="black" underline="none" href="credits">
-            Credits
-          </Link>
-        </Stack>
-
-        <Accordion
-          sx={{
-            backgroundColor: '#8f8c8c',
-            color: 'white',
-            border: '3px solid gray',
-          }}
-          className="disclaimer"
-        >
-          <AccordionSummary
-            expandIcon={
-              <ExpandMoreIcon sx={{ color: 'white', fontSize: '2rem' }} />
-            }
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography sx={{ fontSize: '1.5em' }}>Disclaimer</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              BroncoDirectMe is not created by, managed by, endorsed by, or
-              affiliated with Cal Poly Pomona or the California State University
-              system. BroncoDirectMe is a 100% student run open source project.
-              We do not warrant the information provided on this website for
-              accuracy or relevancy. For clarification on the information
-              provided, please utilize official CPP resources and datasources.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-      </footer>
+      <Footer />
     </div>
   );
 }
